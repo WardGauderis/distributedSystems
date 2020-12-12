d2a5b75e9a4dccf3a839c327e07124c5e0056c1e
Ward <ward.gauderis@student.uantwerpen.be>
2020-12-12 22:06:37 +0100

    l

--- project/app/frontend/frontend.py
+++ project/app/frontend/frontend.py
@@ -16,14 +16,16 @@ def error_handler(error):
 @bp.route('/', methods=['GET'], defaults={'season': 0})
 @bp.route('/<int:season>', methods=['GET'])
 def index(season):
+	divisions = requests.get(f'http://nginx/api/crud/divisions').json()
 	stats = requests.get(f'http://nginx/api/stats/top/{season}').json()
 	seasons = requests.get('http://nginx/api/crud/seasons').json()
-	return render_template('index.html', stats=stats, seasons=seasons, season=season)
+	return render_template('index.html', stats=stats, seasons=seasons, season=season, divisions=divisions)
 
 
 @bp.route('/divisions/<int:div>/<int:team>/<int:season>', methods=['GET'])
 def divisions(div, team, season):
 	print(request.args)
+	divisions = requests.get(f'http://nginx/api/crud/divisions').json()
 	division = requests.get(f'http://nginx/api/crud/divisions/{div}').json()
 	seasons = requests.get(f'http://nginx/api/crud/seasons').json()
 
@@ -34,7 +36,7 @@ def divisions(div, team, season):
 	fixtures = {week: [fixture for fixture in fixtures if int(fixture['matchweek']) == week] for week in weeks}
 
 	return render_template('division.html', table=table, fixtures=fixtures, seasons=seasons, division=division,
-						   season=season, team=str(team))
+						   season=season, team=str(team), divisions=divisions)
 
 
 @bp.route('/teams/<int:id>', methods=['GET'])
