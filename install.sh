b725f69a8e9599f69725fcf7e7be5cb309b79f24
Ward <ward.gauderis@student.uantwerpen.be>
2020-12-18 15:15:16 +0100

    m

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 # docker-compose is verreist om de webapp op te starten
 # ik veronderstel dat de dit scriptje wordt uitgevoerd op een systeem met de apt package manager
-sudo apt install docker-compose
+# ik veronderstel eveneens dat de gebruiker correct gemachtigd is
+apt install docker-compose
+systemctl enable --now docker.service
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 cd project
+echo ik veronderstel dat de gebruiker correct gemachtigd is
 docker-compose up -d
+echo de website is bereikbaar op localhost
