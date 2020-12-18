ac25e504f3817849d3f6fe7e36e6cb827f0fef97
Ward <ward.gauderis@student.uantwerpen.be>
2020-12-18 20:04:14 +0100

    sfd

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # docker-compose is verreist om de webapp op te starten
-echo ik veronderstel dat de dit scriptje wordt uitgevoerd op een systeem met de apt package manager
+echo ik veronderstel dat de dit scriptje wordt uitgevoerd op een systeem met de apt package manager en de systemd service manager
 echo ik veronderstel eveneens dat de gebruiker correct gemachtigd is
 apt install docker-compose
 systemctl enable --now docker.service
